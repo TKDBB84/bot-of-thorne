8bd3056803d2a2970d4c924abec721471995d0d8
Jeffrey Campbell <jeffrey.campbell@autodesk.com>
2022-09-10 11:29:58 -0400

    with pre-pop there's no reason for claim

--- src/slash-commands/claim/index.ts
+++
@@ -1,5 +0,0 @@
-import claimCommand from './command.js';
-import claimRegistrationData from './registration-data.js';
-
-export const command = claimCommand;
-export const registrationData = claimRegistrationData;
--- src/slash-commands/index.ts
+++ src/slash-commands/index.ts
@@ -1,7 +1,6 @@
 import { command as DaysCommand, registrationData as DaysRegistration } from './days/index.js';
 import { command as PingCommand, registrationData as PingRegistration } from './ping/index.js';
-import { command as ClaimCommand, registrationData as ClaimRegistrationData } from './claim/index.js';
 
 export * from './types.js';
-export const registrationData = [DaysRegistration, PingRegistration, ClaimRegistrationData];
-export default [DaysCommand, PingCommand, ClaimCommand];
+export const registrationData = [DaysRegistration, PingRegistration];
+export default [DaysCommand, PingCommand];
