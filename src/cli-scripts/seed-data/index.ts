033a07c9b82922a8bdcf61f1346ee4bac42dcbcf
Jeffrey Campbell <jeffrey.campbell@autodesk.com>
2022-09-16 18:52:43 -0400

    remove seed

--- src/cli-scripts/seed-data/index.ts
+++ src/cli-scripts/seed-data/index.ts
@@ -1,36 +1,7 @@
-import dataSource from '../../data-source.js';
 import logger from '../../logger.js';
-import { Character /*, User */ } from '../../entities/index.js';
-import dayjs from 'dayjs';
-import { CoTAPIId } from '../../consts.js';
-// import users from './user-data.js';
 
-const charRepo = dataSource.getRepository(Character);
-// const userRepo = dataSource.getRepository(User);
 async function main() {
-  // const newUsers: User[] = [];
-  // for (const sbUser of users) {
-  //   newUsers.push(
-  //     userRepo.create({
-  //       id: sbUser.discordUserId,
-  //       timezone: sbUser.timezone || 'Etc/UTC',
-  //     }),
-  //   );
-  // }
-  // await userRepo.save(newUsers);
-
-  const allChars = await charRepo.find()
-  const toUpdate: Character[] = []
-  for (const char of allChars) {
-    const shouldBeLastSeen = dayjs(new Date('2022-09-15'))
-    const thisLastSeen = dayjs(char.last_seen_in_fc)
-    if (thisLastSeen.isAfter(shouldBeLastSeen)) {
-      char.free_company_id = CoTAPIId
-      char.free_company_name = 'Crowne of Thorne'
-      toUpdate.push(char)
-    }
-  }
-  await charRepo.save(toUpdate)
+  // use this if you want to seed data, or something
 }
 
 main()
